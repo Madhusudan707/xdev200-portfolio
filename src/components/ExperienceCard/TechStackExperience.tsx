88bbe667044dda937d82da35c2265001f459ee4c
XDEV200 <xdev200200@gmail.com>
2023-09-13 09:31:44 +0530

    update: techstack font style

--- src/components/ExperienceCard/TechStackExperience.tsx
+++ src/components/ExperienceCard/TechStackExperience.tsx
@@ -6,7 +6,10 @@ export const TechStackExperience: React.FC<TechStackExperienceProps> = ({
 }) => {
   return (
     <FlexBoxContainer className="flex-col py-1 lg:w-3/12 lg:gap-y-0 gap-y-2">
-      <Paragraph text="TechStack" className="text-xs lg:text-center " />
+      <Paragraph
+        text="TechStack"
+        className="text-xs lg:text-center font-bold "
+      />
       <FlexBoxContainer className="flex-wrap lg:justify-center ">
         {techStack.map((item) => (
           <Chip key={item} className="bg-white text-black font-light">
